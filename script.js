fd1b83682895cda760f6f5c2bc96847ea6242b44
codinglil <102619991+codinglil@users.noreply.github.com>
2022-12-24 03:43:51 -0500

    addded hidden class to textarea and .main

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function addNewNote(text = "") {
         <button class="edit"><i class="fas fa-edit"></i></button>
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
       </div>
-      <div class="main"></div>
-      <textarea></textarea>
+      <div class="main ${text ? "" : "hidden"}"></div>
+      <textarea class="${text ? "hidden" : ""}"></textarea>
     </div>`;
 }
